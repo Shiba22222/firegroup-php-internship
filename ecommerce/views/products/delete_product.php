350a56d2a44e4fc3c07317fccf0b0b7644745906
Phạm Kính <57314530+Shiba22222@users.noreply.github.com>
2022-06-16 23:57:04 +0700

    Update delete_product.php

--- ecommerce/views/products/delete_product.php
+++ ecommerce/views/products/delete_product.php
@@ -3,18 +3,48 @@
     
 ?>
 
-
 <?php
     if(isset($_GET['product_id'])){
-        //TODO: Role = admin 
-        $id = $_GET['product_id'];
-        $getUserID = $_COOKIE['user_id'];
-        $getUserRole = $_COOKIE['role'];
-        $sqlProduct = "DELETE FROM products WHERE product_id='$id'";
-        $resProduct = mysqli_query($connect, $sqlProduct);
-        if($resProduct == true){
-            $_SESSION['delete_product'] = "<font color='blue'>Xóa sản phẩm thành công!</font>";
+        $adminRole = $_COOKIE['role'];
+        $adminID = $_COOKIE['user_id'];
+        $productID = $_GET['product_id'];
+        $productSql = "SELECT * FROM products
+                        inner join users on users.user_id = products.user_id
+                        WHERE product_id = '$productID'";
+        $productRes = mysqli_query($connect, $productSql);
+        $productData = mysqli_fetch_assoc($productRes);
+        if(empty($productData)){
+            echo '<h2>Khong tim thay san pham</h2>';
+        }
+        $productUserRole = $productData['role'];
+        $productUserId = $productData ['user_id'];
+
+        $isDelete = false;
+        
+        if($productUserRole == 'admin' && $productUserId == $adminID){  
+            $isDelete = true;
+        }
+        if($productUserRole == 'user' && $productUserId == $adminID){ 
+            $isDelete = true;
+        }
+        if($productUserRole == 'user' && $adminRole == 'admin'){ 
+            $isDelete = true;
+        }
+        
+        if($isDelete){
+            $productDelete = "DELETE FROM products where product_id = '$productID'";
+            $productDeleteRes = mysqli_query($connect, $productDelete);
+            if($productDeleteRes == false){
+                echo "Xoa khong thanh cong";
+            }else{
+                header("location: product.php");
+            }
+        }else{
+            echo "Ban khong duoc xoa du lieu";
             header("location: product.php");
-        }else { echo "Có lỗi!";}
+        }
+        
+        
     }
+    
 ?>
